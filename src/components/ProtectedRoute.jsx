a232ad1978d29e71cd276d8809aacca7726dc5fd
LeoPedroso2323 <leonardopedroso857@gmail.com>
2025-08-26 15:18:15 -0300

    Implementação rotas protegidas

---
+++ src/components/ProtectedRoute.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Navigate } from "react-router-dom";
+
+function ProtectedRoute({ children }) {
+  const token = localStorage.getItem("token");
+
+  // se token não existir ou for inválido → redireciona
+  if (!token || token === "undefined" || token === "null") {
+    return <Navigate to="/login" replace />;
+  }
+
+  return children;
+}
+
+export default ProtectedRoute;
